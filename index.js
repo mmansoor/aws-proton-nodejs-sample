cf8416d0532d7078171c3ea5dcd1acb3ba78b2c2
mubbashir mustafa <mubbashir10@gmail.com>
2020-12-04 08:31:36 +0500

    chores: test updates

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = new express();
 
 app.get("/", (req, res) =>
-  res.send({ status: "success", message: "proton ❤️" })
+  res.send({ status: "✅ success", message: "proton ❤️" })
 );
 
 app.listen(80, () => console.log("server listening on 80"));
